cacbb099bc462654fc0929331e6ef54d220f3a21
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-07-08 10:40:29 +0900

    Define consts for exit codes

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import "os"
 
+const (
+	ExitCodeOK int = iota
+	ExitCodeError
+)
+
 func main() {
 	os.Exit(Run(os.Args))
 }
 
 func Run(args []string) int {
-	return 0
+	return ExitCodeOK
 }
