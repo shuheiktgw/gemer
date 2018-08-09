18cc760bfee6e7fc88b575b6682923ee1bf6674a
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-08-09 09:15:39 +0900

    Remove unecessary go get from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -eu
 
-go get github.com/Songmu/goxz/cmd/goxz
-go get github.com/tcnksm/ghr
-
 # Extract value of Version const from version.go
 VERSION=`grep 'Version =' version.go | sed -E 's/.*"(.+)"$$/\1/'`
 
